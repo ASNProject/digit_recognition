7c2f28e09aace44197ba18617c14ea65a7b177a2
Arief Setyo Nugroho <49858542+ASNProject@users.noreply.github.com>
2023-12-09 14:40:54 +0700

    update creating camera

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import requests
 from flask import Flask, render_template, Response
 import cv2
+import numpy as np
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'capture/images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
@@ -23,10 +28,24 @@ def gen():
     cv2.destroyAllWindows()
 
 
+def save_images(image):
+    filename = '1.jpg'
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    cv2.imwrite(filepath, image)
+
+
 @app.route('/video_feed')
 def video_feed():
     """Video streaming"""
     return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def save_image_route():
+    content = requests.get_data()
+    nparr = np.frombuffer(content, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    save_images(image)
+    return 'Gambar berhasil disimpan'
 
 
 if __name__ == '__main__':
